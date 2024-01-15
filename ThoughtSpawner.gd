654ced2a6c029f449df170983020b0fc58358aa0
elle <elle@catgirl.software>
2024-01-15 16:19:39 +1030

    fast thoughts!

--- ThoughtSpawner.gd
+++ ThoughtSpawner.gd
@@ -3,10 +3,15 @@ extends Node2D
 var THOUGHT := preload("res://thought.tscn")
 var rng := RandomNumberGenerator.new()
 
-func spawn(min_speed, max_speed):
+func spawn(min_speed, max_speed, fast_min, fast_max, fast_chance):
 	var thought := THOUGHT.instantiate()
-	thought.modulate = thought.modulate.darkened(0.5)
-	thought.speed = rng.randf_range(min_speed, max_speed)
+
+	if rng.randf() < fast_chance:
+		thought.speed = rng.randf_range(fast_min, fast_max)
+		thought.modulate = Color.DARK_RED
+	else:
+		thought.speed = rng.randf_range(min_speed, max_speed)
+		thought.modulate = thought.modulate.darkened(0.5)
 	thought.position.x = rng.randf_range(0.0, get_viewport_rect().size.x)
 	add_child(thought)
 
--- main.gd
+++ main.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var min_speed : float
 var max_speed : float
+var fast_min : float
+var fast_max : float
+var fast_chance : float
 var spawn_interval : float
 
 var timer := 0
@@ -10,10 +13,16 @@ var counter := 0
 func _ready():
 	$MinSpeed.value_changed.connect(update_min)
 	$MaxSpeed.value_changed.connect(update_max)
+	$FastMinSpeed.value_changed.connect(update_fast_min)
+	$FastMaxSpeed.value_changed.connect(update_fast_max)
+	$FastChance.value_changed.connect(update_fast_chance)
 	$SpawnInterval.value_changed.connect(update_spawn)
 	update_min(false)
 	update_max(false)
 	update_spawn(false)
+	update_fast_min(false)
+	update_fast_max(false)
+	update_fast_chance(false)
 
 	$StartButton.pressed.connect(start)
 	$StopButton.pressed.connect(stop)
@@ -25,7 +34,7 @@ func _ready():
 	$Clock.timeout.connect(clock)
 
 func spawn():
-	$ThoughtSpawner.spawn(min_speed, max_speed)
+	$ThoughtSpawner.spawn(min_speed, max_speed, fast_min, fast_max, fast_chance)
 	counter += 1
 	update_stats()
 
@@ -52,6 +61,18 @@ func update_min(a):
 func update_max(a):
 	max_speed = $MaxSpeed.value
 	$MaxLabel.text = "max speed: " + str(max_speed)
+
+func update_fast_min(a):
+	fast_min = $FastMinSpeed.value
+	$FastMinLabel.text = "fast thought min speed: " + str(fast_min)
+
+func update_fast_max(a):
+	fast_max = $FastMaxSpeed.value
+	$FastMaxLabel.text = "fast thought max speed: " + str(fast_max)
+
+func update_fast_chance(a):
+	fast_chance = $FastChance.value
+	$FastChanceLabel.text = "fast thought chance: " + str(fast_chance*100) + "%"
 
 func update_spawn(a):
 	spawn_interval = $SpawnInterval.value
